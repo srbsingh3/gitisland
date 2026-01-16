76f058869c74f951bd7239c062b6ce8cf13956bf
Saurabh Singh <32899793+srbsingh3@users.noreply.github.com>
2026-01-17 01:36:39 +0530

    Initial Commit

---
+++ gitisland/gitislandApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitislandApp.swift
+//  gitisland
+//
+//  Created by Saurabh on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct gitislandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
